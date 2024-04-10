c6aee6440c7fe0bab8d92304deca5a4f825883bc
Pinto Tetteh Aaron <aaronpinto111@gmail.com>
2024-04-10 08:15:23 +0000

    update db host

--- social/settings.py
+++ social/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         'NAME': os.getenv('POSTGRES_DB'),
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': os.getenv('POSTGRES_HOST'),
+        'HOST': 'db', # Use the name of the service in the docker-compose file
         'PORT': '5432',
     }
 }
